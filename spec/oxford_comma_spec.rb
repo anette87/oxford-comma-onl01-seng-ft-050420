ff44850f8770327354384adf13257f27c155e4a0
Anette Rivera <anettem.rivera@gmail.com>
2020-05-06 18:22:49 +0000

    Automatically backed up by Learn

--- spec/oxford_comma_spec.rb
+++ spec/oxford_comma_spec.rb
@@ -14,9 +14,20 @@ describe "#oxford_comma" do
   end
 end
 
-def oxford_comma(arr)
-  arr.join
-  arr.join(" and ")
+# def oxford_comma(arr)
+#   arr.join
+#   arr.join(" and ")
   
 end 
 
+def oxford_comma(array)
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
+  end
+end
