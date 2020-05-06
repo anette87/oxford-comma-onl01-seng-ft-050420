83f0dd43b552e3f6ff4b76104830a662838aa012
Anette Rivera <anettem.rivera@gmail.com>
2020-05-06 18:25:37 +0000

    Automatically backed up by Learn

--- spec/oxford_comma_spec.rb
+++ spec/oxford_comma_spec.rb
@@ -14,20 +14,14 @@ describe "#oxford_comma" do
   end
 end
 
-# def oxford_comma(arr)
-#   arr.join
-#   arr.join(" and ")
+ def oxford_comma(arr)
+   arr.join
+   arr.join(" and ")
+    if arr.length == 2
+    arr[-2] << " and "
+    array.join
+  end 
   
-#end 
+end 
 
-def oxford_comma(array)
-  if array.length == 2
-  array[-2] << " and "
-  array.join
-  elsif array.length == 1
-  	array.join
-  elsif array.length > 2
-    array[-1].prepend "and "
-	array.join(", ")
-  end
-end
+ 
